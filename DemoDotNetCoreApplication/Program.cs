9afb3bb3a146922355003ad0500419a2999766dd
Nikhil Ausekar <nikhil.a@pravaltech.com>
2024-09-12 21:27:56 +0530

    added image to docker

--- DemoDotNetCoreApplication/Program.cs
+++ DemoDotNetCoreApplication/Program.cs
@@ -1,38 +1,48 @@
 using DemoDotNetCoreApplication.Contracts;
 using DemoDotNetCoreApplication.Providers;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Data;
+using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddDbContext<DbContextProvider>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ITaskItemProvider, TaskItemProvider>();
-builder.Services.AddScoped<IEmployeeProvider, EmployeeProvider>();
-builder.Services.AddAutoMapper(typeof(AutoMapperProvider));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddDbContext<DbContextProvider>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+        });
+        builder.Services.AddScoped<ITaskItemProvider, TaskItemProvider>();
+        builder.Services.AddScoped<IEmployeeProvider, EmployeeProvider>();
+        builder.Services.AddAutoMapper(typeof(AutoMapperProvider));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}"
-  );
-
-app.Run();
