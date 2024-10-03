a5089f2268ef59304ed6ef03095d5ad008f725ac
Nikhil Ausekar <nikhil.a@pravaltech.com>
2024-10-03 13:54:56 +0530

    new modals

---
+++ DemoDotNetCoreApplication/Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoDotNetCoreApplication.Models;
+
+public partial class Task
+{
+    public int Id { get; set; }
+
+    public DateOnly? AssignedOnDt { get; set; }
+
+    public DateOnly? CreatedOnDt { get; set; }
+
+    public int? EmployeeId { get; set; }
+
+    public DateOnly? EndDate { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual Employee? Employee { get; set; }
+}
