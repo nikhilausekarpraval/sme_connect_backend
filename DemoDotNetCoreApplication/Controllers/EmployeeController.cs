d91a8a48c033c0f46ac35fcb23c5d5949976a415
Nikhil Ausekar <nikhil.a@pravaltech.com>
2024-10-03 14:37:27 +0530

    added services

--- DemoDotNetCoreApplication/Controllers/EmployeeController.cs
+++ DemoDotNetCoreApplication/Controllers/EmployeeController.cs
@@ -10,7 +10,7 @@ namespace DemoDotNetCoreApplication.Controllers
 {
 
     [ApiController]
-    [Authorize(Roles = "Admin,User")]
+    //[Authorize(Roles = "Admin,User")]
     [Route("employee")]
     public class EmployeeController : ControllerBase
     {
--- DemoDotNetCoreApplication/Program.cs
+++ DemoDotNetCoreApplication/Program.cs
@@ -1,77 +1,25 @@
 using DemoDotNetCoreApplication.Contracts;
 using DemoDotNetCoreApplication.Data;
-using DemoDotNetCoreApplication.Modals;
-using DemoDotNetCoreApplication.Modals.JWTAuthentication.Authentication;
 using DemoDotNetCoreApplication.Providers;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Database context setup
 builder.Services.AddDbContext<DcimDevContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers();
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<DcimDevContext>()
-    .AddDefaultTokenProviders();
+        .AddEntityFrameworkStores<DcimDevContext>();
 
-
-
-//builder.Services.AddAuthorization(options => {
-//    options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
-//});
-
-
-
-
-//builder.Services.AddAuthorization();
-
-//builder.Services.AddAuthorization(options =>
-//{
-//    options.AddPolicy("AllUsers", Policy => Policy.RequireClaim("UserAccess"));
-//});
-
-//builder.Services.AddDefaultIdentity<ApplicationUser>()
-//    .AddRoles(ApplicationRole);
-
-builder.Services.AddRazorPages();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Auth Demo", Version = "v1" });
-
-//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-//    {
-//        In = ParameterLocation.Header,
-//        Description = "Please enter a token",
-//        Name = "Authorization",
-//        Type = SecuritySchemeType.Http,
-//        BearerFormat = "JWT",
-//        Scheme = "bearer"
-//    });
-
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-//    {
-//        {
-//            new OpenApiSecurityScheme
-//            {
-//                Reference = new OpenApiReference
-//                {
-//                    Type = ReferenceType.SecurityScheme,
-//                    Id = "Bearer"
-//                }
-//            }, new string[] { }
-//        }
-//    });
-//});
-
-builder.Services.AddScoped<ITaskItemProvider, TaskItemProvider>();
-builder.Services.AddScoped<IEmployeeProvider, EmployeeProvider>();
-builder.Services.AddAutoMapper(typeof(AutoMapperProvider));
+builder.Services.AddScoped<IEmployeeProvider,EmployeeProvider>();
+builder.Services.AddScoped<ITaskItemProvider,TaskItemProvider>();
+builder.Services.AddScoped<RoleProvider>();
+builder.Services.AddScoped<AutoMapperProvider>();
 
 builder.Services.AddCors(options =>
 {
@@ -81,36 +29,30 @@ builder.Services.AddCors(options =>
                           .AllowAnyHeader());
 });
 
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
-    });
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    await RoleProvider.SeedRolesAsync(services);
-}
-
 app.UseHttpsRedirection();
-
-app.UseCors("AllowAllOrigins");
-
+app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
 
-//app.MapControllers();
-
-//app.MapIdentityApi<IdentityUser>();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
+
+
+
+
+
